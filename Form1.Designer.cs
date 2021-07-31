382dcef84478353d545f65aceebb9c5af185c92d
Muhamed Smailhodzic <muhaofficial12@gmail.com>
2021-07-31 18:54:21 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeNowTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblHour = new System.Windows.Forms.Label();
+            this.comboBoxHour = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxMinutes = new System.Windows.Forms.ComboBox();
+            this.btnSetAlarm = new System.Windows.Forms.Button();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TimeNowTimer
+            // 
+            this.TimeNowTimer.Interval = 1000;
+            this.TimeNowTimer.Tick += new System.EventHandler(this.TimeNowTimer_Tick);
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHour.Location = new System.Drawing.Point(97, 84);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(79, 34);
+            this.lblHour.TabIndex = 1;
+            this.lblHour.Text = "Hour";
+            // 
+            // comboBoxHour
+            // 
+            this.comboBoxHour.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxHour.FormattingEnabled = true;
+            this.comboBoxHour.Location = new System.Drawing.Point(181, 86);
+            this.comboBoxHour.Name = "comboBoxHour";
+            this.comboBoxHour.Size = new System.Drawing.Size(121, 32);
+            this.comboBoxHour.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Min";
+            // 
+            // comboBoxMinutes
+            // 
+            this.comboBoxMinutes.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMinutes.FormattingEnabled = true;
+            this.comboBoxMinutes.Location = new System.Drawing.Point(181, 137);
+            this.comboBoxMinutes.Name = "comboBoxMinutes";
+            this.comboBoxMinutes.Size = new System.Drawing.Size(121, 32);
+            this.comboBoxMinutes.TabIndex = 6;
+            // 
+            // btnSetAlarm
+            // 
+            this.btnSetAlarm.BackColor = System.Drawing.Color.Transparent;
+            this.btnSetAlarm.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetAlarm.ForeColor = System.Drawing.Color.Black;
+            this.btnSetAlarm.Location = new System.Drawing.Point(168, 204);
+            this.btnSetAlarm.Name = "btnSetAlarm";
+            this.btnSetAlarm.Size = new System.Drawing.Size(149, 44);
+            this.btnSetAlarm.TabIndex = 8;
+            this.btnSetAlarm.Text = "Set Alarm";
+            this.btnSetAlarm.UseVisualStyleBackColor = false;
+            this.btnSetAlarm.Click += new System.EventHandler(this.btnSetAlarm_Click);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(12, 9);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(47, 35);
+            this.lblHours.TabIndex = 9;
+            this.lblHours.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 35);
+            this.label2.TabIndex = 10;
+            this.label2.Text = ":";
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(72, 9);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(47, 35);
+            this.lblMinutes.TabIndex = 11;
+            this.lblMinutes.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(116, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 35);
+            this.label3.TabIndex = 12;
+            this.label3.Text = ":";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(132, 9);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(47, 35);
+            this.lblSeconds.TabIndex = 13;
+            this.lblSeconds.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.btnSetAlarm);
+            this.Controls.Add(this.comboBoxMinutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxHour);
+            this.Controls.Add(this.lblHour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer TimeNowTimer;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.ComboBox comboBoxHour;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxMinutes;
+        private System.Windows.Forms.Button btnSetAlarm;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSeconds;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        int hour, minute, second;
+        string alarmHour, alarmMinute;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeNowTimer.Start();
+
+            for (int hrs = 0; hrs < 24; hrs++)
+            {
+                comboBoxHour.Items.Add(hrs);
+            }
+
+            for(int mins = 0; mins<60; mins++)
+            {
+                comboBoxMinutes.Items.Add(mins);
+            }
+        }
+
+        private void TimeNowTimer_Tick(object sender, EventArgs e)
+        {
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
+
+            lblHours.Text = hour.ToString();
+            lblMinutes.Text = minute.ToString();
+            lblSeconds.Text = second.ToString();
+
+            Ring_Alarm();
+        }
+
+        private void btnSetAlarm_Click(object sender, EventArgs e)
+        {
+            alarmHour = comboBoxHour.Text;
+            alarmMinute = comboBoxMinutes.Text;
+        }
+
+        void Ring_Alarm()
+        {
+            if(alarmHour==hour.ToString() && alarmMinute==minute.ToString() && second.ToString() == "0")
+            {
+                MessageBox.Show("Sound");
+            }
+        }
+    }
+}
